47377e186327c0d3baffd4dec234461a7abe1dc6
Bakhita87 <bakhitaotieno@gmail.com>
2023-08-23 21:11:11 +0300

    Completed assignment

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,21 @@
 # your code goes here!
+import time  # Import the time module for the sleep function
+
+def countdown(seconds):
+    while seconds > 0:
+        print(f'{seconds} SECOND(S)!')
+        seconds -= 1
+    print('HAPPY NEW YEAR!')
+
+def countdown_with_sleep(seconds):
+    while seconds > 0:
+        print(f'{seconds} SECOND(S)!')
+        time.sleep(1)  # Sleep for one second
+        seconds -= 1
+    print('HAPPY NEW YEAR!')
+
+# Example usage
+countdown(10)
+print("===")
+countdown_with_sleep(10)
+
